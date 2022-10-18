ae9f1233de77d2341c4054bde8ee63ad8e669fba
Jechol Lee <mr.jechol@gmail.com>
2022-10-18 20:05:31 +0900

    override_def

--- test/rail_test.exs
+++ test/rail_test.exs
@@ -37,4 +37,26 @@ defmodule RailTest do
 
     assert {:ok, 3} == result
   end
+
+  defmodule Calc2 do
+    use Rail, override_def: true
+
+    def div(num, denom) do
+      denom <- check_denom(denom)
+      num / denom
+    end
+
+    def check_denom(0) do
+      {:error, :div_by_zero}
+    end
+
+    def check_denom(n) do
+      {:ok, n}
+    end
+  end
+
+  test "def/2" do
+    assert {:ok, 5} == Calc2.div(10, 2)
+    assert {:error, :div_by_zero} == Calc2.div(10, 0)
+  end
 end
