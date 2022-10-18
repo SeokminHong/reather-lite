acfdbc7d6f4913863177ddae86455ee699e03f9f
Jechol Lee <mr.jechol@gmail.com>
2022-10-18 19:24:40 +0900

    rename

--- lib/rail.ex
+++ lib/rail.ex
@@ -1,11 +1,67 @@
 defmodule Rail do
-  require Rail.Macros
-
   defmacro __using__([]) do
     quote do
-      import Rail.Macros, only: [rail: 1, rail: 2, railp: 2]
-      require Rail.Macros
+      import Rail, only: [rail: 1, rail: 2, railp: 2]
       alias Rail.Either
     end
+  end
+
+  alias Rail.Either
+
+  defmacro rail(head, body) do
+    expanded_body = expand_body(body)
+
+    quote do
+      def unquote(head), unquote(expanded_body)
+    end
+  end
+
+  defmacro railp(head, body) do
+    expanded_body = expand_body(body)
+
+    quote do
+      defp unquote(head), unquote(expanded_body)
+    end
+  end
+
+  defmacro rail([do: _] = body) do
+    expand_body(body)
+  end
+
+  # Private
+
+  defp expand_body([{:do, do_block} | rest]) do
+    [{:do, expand_do_block(do_block)} | rest]
+  end
+
+  defp expand_do_block({:__block__, _ctx, exprs}) do
+    parse_exprs(exprs)
+  end
+
+  defp expand_do_block(expr) do
+    parse_exprs([expr])
+  end
+
+  defp parse_exprs(exprs) do
+    {body, ret} = Enum.split(exprs, -1)
+
+    wrapped_ret =
+      quote do
+        unquote(List.first(ret)) |> Either.new()
+      end
+
+    body
+    |> List.foldr(wrapped_ret, fn
+      {:<-, _ctx, [lhs, rhs]}, acc ->
+        quote do
+          unquote(rhs) |> Either.chain(fn unquote(lhs) -> unquote(acc) end)
+        end
+
+      expr, acc ->
+        quote do
+          unquote(expr)
+          unquote(acc)
+        end
+    end)
   end
 end
